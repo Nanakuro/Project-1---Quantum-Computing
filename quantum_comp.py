8ab7dc751a5357c30a8dab3c70cf330ab298362f
Minh Nguyen <hjhjhaha21@gmail.com>
2019-01-24 18:41:56 -0600

    Constructed Hadamard and Phase gate

--- quantum_comp.py
+++ quantum_comp.py
@@ -20,7 +20,7 @@ def PrettyPrintBinary(state):
 def PrettyPrintInteger(state):
     i = 1
     for s in state:
-        num = int(s[1],2)
+        num = int(s[1],2)   # Convert binary string to base-10 integer
         if i == len(state):
             print('{:g} |{:g}>'.format(s[0], num))
         else:
@@ -41,29 +41,73 @@ def VecToState(vector):
     for i in range(len(vector)):
         v = vector[i]
         if v != 0.0:
-            basis = "{0:b}".format(i)
+            basis = "{0:b}".format(i)   # Convert base-10 integer to binary string
             state.append([v,basis])
             l = len(basis)
     for s in state:
         s[1] = s[1].zfill(l)
     state = [tuple(s) for s in state]
     return state
-            
-        
+         
+##############################
+
+   
+def tensorProd(matrix_list):
+    product = matrix_list[0]
+    i = 1
+    while i < len(matrix_list):
+        product = np.kron(matrix_list[i],product)
+        i += 1
+    return product
+
+def HadamardArray(i,k):     # Apply Hadamard matrix to wire i out of k wires
+    Hadamard = 1/np.sqrt(2) * np.array([[1,1],[1,-1]])
+    M_list = []
+    for j in range(k):
+        if j == k-i-1:
+            M_list.append(Hadamard)
+            continue
+        M_list.append(np.identity(2))
+    
+    matrix = tensorProd(M_list)
+    print(matrix)
+    return matrix
+
+def PhaseArray(i,k,phi):
+    Phase = np.array([[1,0],[0,np.exp(phi*1.j)]])
+    M_list = []
+    for j in range(k):
+        if j == k-i-1:
+            M_list.append(Phase)
+            continue
+        M_list.append(np.identity(2))
+    
+    matrix = tensorProd(M_list)
+    print(matrix)
+    return matrix
+
+def CNOT(i,j,k):
     
 
 
 
 
-testState = [
-        (np.sqrt(0.1)*1.j, '101'),
-        (np.sqrt(0.5), '000') ,
-        (-np.sqrt(0.4), '010' )
-        ]
+#testState = [
+#        (np.sqrt(0.1)*1.j, '101'),
+#        (np.sqrt(0.5), '000') ,
+#        (-np.sqrt(0.4), '010' )
+#        ]
+#testState2 = [
+#        (np.sqrt(0.1)*3.j+2, '111'),
+#        (np.sqrt(0.5)-0.1, '110') ,
+#        (-np.sqrt(0.4)-2.j, '001' )
+#        ]
 
-#PrettyPrintBinary(testState)
-#PrettyPrintInteger(testState)
+#PrettyPrintBinary(testState2)
+#PrettyPrintInteger(testState2)
 
-print(StateToVec(testState))
-print(VecToState(StateToVec(testState)))
+#print(StateToVec(testState2))
+#print(VecToState(StateToVec(testState2)))
 
+HadamardArray(1,2)
+PhaseArray(0,2,0.1)
